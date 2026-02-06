3dd754e132768d303e32a5de647042dae0d4557c
Bungus <dmychel@tutanota.com>
2026-02-06 13:37:45 -0500

    created baseline jsx  and imported sass module

--- src/components/Culture.jsx
+++ src/components/Culture.jsx
@@ -1,6 +1,16 @@
+import styles from '/src/styles/gridContainer.module.scss'
 function Culture() {
     return (
-        <div className="culture"></div>
+        <div className={styles.culture}>
+            <h1>Live the Culture</h1>
+            <div className={styles.gridContainer}>
+                <img src="/assets/images/1.jpg" alt="photo" />
+                <img src="/assets/images/2.jpg" alt="photo" />
+                <img src="/assets/images/3.jpg" alt="photo" />
+                <img src="/assets/images/4.jpg" alt="photo" />
+                <img src="/assets/images/5.jpg" alt="photo" />
+            </div>
+        </div>
     )
 }
 
