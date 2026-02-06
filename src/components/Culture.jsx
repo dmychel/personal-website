1a9ff6796b0d786e6b95a1be7c267998bc5ab6fb
Bungus <dmychel@tutanota.com>
2026-02-06 14:45:03 -0500

    removed unnecessary nested containers

--- src/components/Culture.jsx
+++ src/components/Culture.jsx
@@ -2,13 +2,12 @@ import styles from '/src/styles/gridContainer.module.scss'
 function Culture() {
     return (
         <div className={styles.culture}>
-            <h1>Live the Culture</h1>
-            <div className={styles.gridContainer}>
-                <img src="/assets/images/1.jpg" alt="photo" />
-                <img src="/assets/images/2.jpg" alt="photo" />
-                <img src="/assets/images/3.jpg" alt="photo" />
+            <div className={styles.column}>
                 <img src="/assets/images/4.jpg" alt="photo" />
                 <img src="/assets/images/5.jpg" alt="photo" />
+                <img src="/assets/images/3.jpg" alt="photo" />
+                <img src="/assets/images/1.jpg" alt="photo" />
+                <img src="/assets/images/2.jpg" alt="photo" />
             </div>
         </div>
     )
