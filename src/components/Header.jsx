da442979d62bec3d2b4566b78edad6471e9ba660
Bungus <dmychel@tutanota.com>
2026-02-09 12:37:39 -0500

    base jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+// styles
+import styles from "/src/styles/header.module.scss"
+
+function Header() {
+    return (
+        <header>
+            <ul>
+                <li>Photography |</li>
+                <li>Traditional Art |</li>
+                <li>Digital Art |</li>
+                <li>About</li>
+            </ul>
+        </header>
+    )
+}
+
+export { Header }
