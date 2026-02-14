10af00553640b6191685bad192d19234e83a6113
Bungus <dmychel@tutanota.com>
2026-02-14 12:14:11 -0500

    creates routes with react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
+// npm package
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// routes
+import { About } from "./routes/About";
+import { Culture } from "./routes/Culture";
+
 // components
 import { Header } from "./components/Header";
-import { Culture } from "./components/Culture";
+
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
       <main>
-        <Culture />
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path='/photography/culture' element={<Culture />} />
+        </Routes>
       </main>
-    </>
+    </BrowserRouter>
 
 
 
