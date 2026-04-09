40a63a66495e8108e7e159b24e0b537c86e97ff7
Bungus <dmychel@tutanota.com>
2026-04-09 10:15:24 -0400

    created dynamic variables

--- src/api/fetch.js
+++ src/api/fetch.js
@@ -1,9 +1,9 @@
 import PropTypes from 'prop-types';
 export const BASE_URL = 'https://dmychel.github.io/photo-gallery'
 
-export async function fetchAPI({ photos, setPhotos }) {
+export async function fetchAPI({ photos, setPhotos }, jsonURL) {
     console.log(photos, setPhotos)
-    const response = await fetch(`${BASE_URL}/culture.json`);
+    const response = await fetch(`${BASE_URL}/${jsonURL}`);
 
     if (!response.ok) {
         throw new Error("Netowrk response was not ok");
