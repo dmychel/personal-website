383156dc58c6e87862282bed3adc543f0c266a0b
Bungus <dmychel@tutanota.com>
2026-04-09 10:19:59 -0400

    created base route

---
+++ src/routes/About.jsx
@@ -0,0 +1,19 @@
+import styles from '/src/styles/about.module.scss';
+
+function About() {
+    return (
+        <div className={styles.about}>
+            <div className={styles.left}>
+                <img src="/assets/images/profile.jpg" alt="Error Loading Image" />
+            </div>
+            <div className={styles.right}>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt voluptatem quas expedita rerum sequi ea temporibus, incidunt aspernatur non quibusdam eveniet laborum! Aliquam totam corrupti recusandae molestiae quo tempore dolores.
+
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export { About }
