b4ed7c49f7d4e4ed26969069378c20db1fd6ef72
Bungus <dmychel@tutanota.com>
2026-04-09 13:03:47 -0400

    changed class names

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -3,10 +3,10 @@ import styles from '/src/styles/about.module.scss';
 function About() {
     return (
         <div className={styles.about}>
-            <div className={styles.left}>
+            <div className={styles.imageContainer}>
                 <img src="/assets/images/profile.jpg" alt="Error Loading Image" />
             </div>
-            <div className={styles.right}>
+            <div className={styles.textContainer}>
                 <p>
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt voluptatem quas expedita rerum sequi ea temporibus, incidunt aspernatur non quibusdam eveniet laborum! Aliquam totam corrupti recusandae molestiae quo tempore dolores.
 
