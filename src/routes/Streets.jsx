17193d2cf38881658ba61af194bed7afdfc227b5
Bungus <dmychel@tutanota.com>
2026-04-13 11:03:18 -0400

    created route

---
+++ src/routes/Streets.jsx
@@ -0,0 +1,29 @@
+// styles
+import { useEffect, useState } from 'react';
+import styles from '/src/styles/gridContainer.module.scss';
+
+// functions
+import { callFetch } from '../api/callFetch';
+
+function Streets() {
+    const [photos, setPhotos] = useState([]);
+
+    useEffect(() => {
+        callFetch({ photos, setPhotos }, 'culture.json')
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
+    return (
+        <div className={styles.culture}>
+            <h1>The Culture</h1>
+            <div className={styles.column}>
+
+                {photos.map((photo, index) => (
+                    < img key={index} src={`https://dmychel.github.io/photo-gallery/photography/streets/${photo.url}.jpg`} alt={photo.url} />
+                ))
+                }
+            </div>
+        </div>
+    )
+}
+export { Streets };
