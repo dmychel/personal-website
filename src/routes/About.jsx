1166e42ecc7e145e37d024fefe7dba7ea0fe0a37
Bungus <dmychel@tutanota.com>
2026-04-26 18:09:48 -0500

    jsx update

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -7,9 +7,7 @@ function About() {
                 <img src="/assets/images/profile.jpg" alt="Error Loading Image" />
             </div>
             <div className={styles.textContainer}>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt voluptatem quas expedita rerum sequi ea temporibus, incidunt aspernatur non quibusdam eveniet laborum! Aliquam totam corrupti recusandae molestiae quo tempore dolores.
-
+                <p>As a versatile photographer, I am driven by a deep passion for capturing compelling stories through live-action, candid moments, and portrait photography. With an unwavering focus on visual storytelling, I specialize in capturing the grace, dynamism, and profound emotions in dance, and human interaction.
                 </p>
             </div>
         </div>
