d8385f82f7a4e1ff6a61df2cf2437ca1c07b8633
Bungus <dmychel@tutanota.com>
2026-04-26 18:09:41 -0500

    imported and rendered NavDropDown component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,17 +4,21 @@ import { Link } from "react-router-dom";
 // styles
 import styles from "/src/styles/header.module.scss";
 
+// functions
+import NavDropDown from "./NavDropDown";
+
 
 function Header() {
     return (
         <header className={styles.header}>
-            <ul>
+            <ul className={styles.headerUL}>
                 <li><Link to='/' >Portraits</Link> |</li>
                 <li><Link to='/photography/streets'>Streets</Link> |</li>
                 <li><Link to='/photography/culture'>Culture</Link> |</li>
                 <li><Link to='/photography/bulletin'>Projects</Link> |</li>
                 <li><Link to='/about'>About</Link></li>
             </ul>
+            <NavDropDown />
         </header>
     )
 }
