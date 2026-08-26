3f9cad4b4858da7ee9de7b1ddd4c6df57ea02e4f
Bungus <dmychel@tutanota.com>
2026-08-25 19:55:54 -0500

    created map function to render array

--- src/routes/Index.jsx
+++ src/routes/Index.jsx
@@ -1,5 +1,25 @@
+import styles from '/src/styles/index.module.scss'
+
 function Index() {
-    return
+    const categories = [
+        {
+            title: 'Portraits',
+            cover: 'https://placehold.co/600x400'
+        }
+    ]
+
+    return (
+        <div className={styles.index}>
+            {
+                categories.map((box, index) => (
+                    <div index={index}>
+                        <h1>{box.title}</h1>
+                    </div>
+                ))
+            }
+
+        </div>
+    )
 }
 
 export { Index }
