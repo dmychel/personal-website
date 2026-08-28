718bfb1f24e6bcc01370f959944e255825d1d008
Bungus <dmychel@tutanota.com>
2026-08-27 22:10:57 -0500

    added link value to objects

--- src/routes/Index.jsx
+++ src/routes/Index.jsx
@@ -6,23 +6,25 @@ function Index() {
             title: '',
             description: 'Portraits',
             cover: '/assets/images/portraits.jpg',
+            link: `/photography/portraits`
 
         },
         {
             title: '',
             description: 'Streets',
             cover: '/assets/images/streets.jpg',
-
+            link: `/photography/streets`
         },
         {
             title: '',
             description: 'RBDYS Boston \'23',
             cover: '/assets/images/culture.jpg',
-
+            link: `/photography/rbdys`
         }, {
             title: '',
             description: 'Brigham & Woman\'s Bulletin',
             cover: '/assets/images/bulletin.jpg',
+            link: `/photography/bulletin`
         },
     ]
 
@@ -33,7 +35,9 @@ function Index() {
                 categories.map((item, index) => (
                     <div className={styles.box} key={index}>
                         {/* <h1 className={styles.title}>{item.title}</h1> */}
-                        <div className={styles.hoverBack}></div>
+                        <div className={styles.hoverBack}>
+                            <a href={item.link}></a>
+                        </div>
                         <div style={{ backgroundImage: `url(${item.cover})` }} className={styles.catBackground}>
                             <p > {item.description}</p>
                         </div>
