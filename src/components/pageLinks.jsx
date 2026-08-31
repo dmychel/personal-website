14a1b5e9fccb0bd688fc79f581f06db95eb67d9c
Bungus <dmychel@tutanota.com>
2026-08-31 18:03:39 -0500

    created component to ensure proper links throughout web app

---
+++ src/components/pageLinks.jsx
@@ -0,0 +1,7 @@
+const portraitURL = '/photography/portraits'
+const streetsURL = '/photography/streets'
+const rbdysURL = '/photography/rbdys'
+const bulletinURL = '/photography/bulletin'
+const aboutURL = '/about'
+
+export { portraitURL, streetsURL, rbdysURL, bulletinURL, aboutURL }
